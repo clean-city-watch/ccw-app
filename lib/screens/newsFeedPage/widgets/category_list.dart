b7a257cd4cc4cb88b0e4b99c2e6f9c0ec7a3c2a0
yash202000 <yashpanchwatkar@gmail.com>
2024-05-03 16:24:12 +0530

    category list order changed

--- lib/screens/newsFeedPage/widgets/category_list.dart
+++ lib/screens/newsFeedPage/widgets/category_list.dart
@@ -4,9 +4,7 @@ import 'package:ccw/screens/organization/listorganizationscreen.dart';
 import 'package:ccw/screens/organization/selectorganizationNavigateState.dart';
 import 'package:flutter/material.dart';
 
-
 class CategoryList extends StatefulWidget {
-
   CategoryList({Key? key}) : super(key: key);
 
   @override
@@ -14,11 +12,10 @@ class CategoryList extends StatefulWidget {
 }
 
 class _CategoryListState extends State<CategoryList> {
-
   List<String> listCategory = [
     'All posts',
+    'Organizations',
     'Leadboard',
-    'Organizations'
   ];
 
   @override
@@ -31,18 +28,23 @@ class _CategoryListState extends State<CategoryList> {
             return GestureDetector(
               onTap: () {
                 debugPrint('${listCategory[index]} Tapped');
-                if(listCategory[index]=='Leadboard'){
-                   Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => LeaderboardScreen()));
-                }else if (listCategory[index]=='Organizations'){
-                   Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => OrganizationState())); 
+                if (listCategory[index] == 'Leadboard') {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => LeaderboardScreen()));
+                } else if (listCategory[index] == 'Organizations') {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => OrganizationState()));
                 }
               },
               child: Container(
                 padding: EdgeInsets.all(8),
                 margin: EdgeInsets.all(8),
-                decoration: index == 0 ? selectedBoxDecoration() : boxDecoration(),
+                decoration:
+                    index == 0 ? selectedBoxDecoration() : boxDecoration(),
                 child: Text(
                   listCategory[index],
                   textAlign: TextAlign.center,
@@ -53,6 +55,4 @@ class _CategoryListState extends State<CategoryList> {
           }),
     );
   }
-
-
 }
